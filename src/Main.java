93e58bfd7c1f1e0a5526b6e2130a3544734459a0
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-01-15 00:23:08 +0300

    task1-3

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,31 @@ public class Main {
     }
 
     public static void task1() {
+        int salary =15_000, total =0, i =1;
         System.out.println("Задача 1");
+        while (total <=2_459_000) {
+            total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+            i++;
+        }
     }
     public static void task2() {
+        int i =1;
         System.out.println("Задача 2");
+        while (i <=10){
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+        for (i =10; i>= 1; i--) System.out.print(i+" ");
     }
     public static void task3() {
-        System.out.println("Задача 3");
+        int population =12_000_000, birthrate =17, mortality =8;
+        System.out.println("\n"+"Задача 3");
+        for (int i =1; i <=10; i++) {
+            population = population + population*(birthrate - mortality)/ 1000;
+            System.out.println("Год " + i + " , численность населения составляет " + population);
+        }
     }
     public static void task4() {
         System.out.println("Задача 4");
